2ccc4b3671a7db9da5e44d6839aa620bff7d2743
Bruno santos <bruno@sequenza.com.br>
2019-01-26 15:19:55 -0200

    using DataContract

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,11 +1,19 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
+﻿using System.Runtime.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
 {
+    [DataContract]
     public class PersonVO
     {
+        [DataMember(Order = 1, Name = "codigo")]
         public long? Id { get; set; }
+        [DataMember(Order = 2)]
         public string FirstName { get; set; }
+        [DataMember(Order = 2)]
         public string LastName { get; set; }
+        [DataMember(Order = 3)]
         public string Address { get; set; }
+        [DataMember(Order = 4)]
         public string Gender { get; set; }
 
     }
